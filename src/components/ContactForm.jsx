175d70a659cbdcafd9cd4020364e2680ae0ff049
amelangad <slodzinska.magdalena@gmail.com>
2024-02-14 11:49:45 +0100

    fix the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -12,9 +12,9 @@ export default function ContactForm() {
             </div>
         </div>
         <div className ="w-full h-screen bg-[#c2afd3] flex flex-col justify-center items-center">
-            <h1 className="  flex justify-center items-center text-2xl font-bold lg:hidden py-20 leading-[0.5px]">Contact</h1>
-            <div className="relative lg:overflow-hidden lg:absolute lg:right-20 w-full md:w-3/4 lg:w-1/2 h-1/2  flex justify-center lg:items-start bg-[#f0eee3] rounded-[20px] md:rounded-tl-[100px]  md:rounded-tr-[350px] md:rounded-bl-[400px] md:rounded-br-[200px]">
-                <motion.h1 className =" hidden lg:flex text-3xl lg:py-20 font-mono"
+            <h1 className="  flex justify-center items-center text-2xl font-bold lg:hidden py-10 leading-[0.5px]">Contact</h1>
+            <div className="relative lg:overflow-hidden lg:absolute lg:right-20 w-full md:w-3/4 lg:w-1/2 h-1/2  flex justify-center lg:items-start bg-[#f0eee3] rounded-[0px] md:rounded-tl-[100px]  md:rounded-tr-[350px] md:rounded-bl-[400px] md:rounded-br-[200px]">
+                <motion.h1 className =" hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-100}}
                 whileInView={{y: 0}}
                 transition={{
@@ -24,7 +24,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     C</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -34,7 +34,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     o</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -44,7 +44,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     n</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -54,7 +54,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     t</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -64,7 +64,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     a</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -74,7 +74,7 @@ export default function ContactForm() {
                     stiffness: 500,
                 }}>
                     c</motion.h1>
-                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono"
+                    <motion.h1 className ="hidden lg:flex text-3xl lg:py-20 font-mono uppercase"
                 initial={{y:-120}}
                 whileInView={{y: 0}}
                 transition={{
@@ -86,24 +86,40 @@ export default function ContactForm() {
                     t</motion.h1>
                 
            
-    <form action="/" className="py-20 lg:py-0 px-5 absolute lg:left-1/3 lg:top-1/4">
-      <div className="grid gap-6 sm:grid-cols-2">
-        <div className="relative z-0">
-          <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your name</label>
-        </div>
-        <div className="relative z-0">
-          <input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your email</label>
-        </div>
-        <div className="relative z-0 col-span-2">
-          <textarea name="message" rows="5"  className="resize-none peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your message</label>
-        </div>
-      </div>
-      <div className ="flex justify-center items-center">
-      <button type="submit" className="mt-5 rounded-md bg-black mx-auto lg:px-10 py-2 mb-5 text-white">Send Message</button>
-      </div>
+    <form action="/" className="w-2/3 max-h-full  absolute  top-[5rem] lg:top-[10rem]">
+     <div className ="flex flex-row justify-center items-end gap-20">
+<div className ="relative z-0">
+   <input type="text"
+   name="name"
+   placeholder=""
+   className ="w-full h-full focus:outline-none px-3 py-3 peer block bg-transparent border-b appearance-none border-black focus:ring-2 "/>
+   <label className ="absolute top-5 -z-10 origin-[0] text-black text-sm -translate-y-3 scale-75 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75">name</label>
+   </div>
+
+   <div className ="relative z-0">
+   <input type="email"
+    placeholder=""
+   name="email"
+   className ="w-full h-full focus:outline-none px-3 py-3 peer block bg-transparent border-b appearance-none border-black focus:ring-2 "/>
+   <label className ="absolute top-5  -z-10 origin-[0] text-black text-sm -translate-y-3 scale-75 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75">email</label>
+   </div>
+     </div>
+     <div className ="relative z-0">
+   <textarea
+    placeholder=""
+   className ="resize-none w-full  focus:outline-none  block px-3 py-10 peer bg-transparent border-b appearance-none border-black focus:ring-2 "/>
+   <label className ="absolute top-5 z-10 origin-[0] text-black text-sm -translate-y-3 scale-75 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-3 peer-focus:scale-75">type your question</label>
+   </div>
+   <motion.button
+    type="submit"
+   className ="bg-black text-white my-5  px-8 py-5 flex justify-center items-center m-auto w-200"
+   whileTap={{scale: .9}}
+   whileHover={{scale: 1.1}}
+   transition={{
+    duration: .5,
+    type: "spring",
+    stiffness: 500,
+}}>send</motion.button>
     </form>
     </div>
             </div>
